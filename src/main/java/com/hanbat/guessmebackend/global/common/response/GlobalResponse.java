7ab8a1a2b9253ab63e1cb3d3f3f515fad46a8aca
suri-pu-bi <ymg12347@gmail.com>
2024-05-13 16:29:25 +0900

    chore: 에러 응답 및 공통 응답 구현

---
+++ src/main/java/com/hanbat/guessmebackend/global/common/response/GlobalResponse.java
@@ -0,0 +1,15 @@
+package com.project.guessme.global.common.response;
+
+import java.time.LocalDateTime;
+
+import com.project.guessme.global.error.ErrorResponse;
+
+public record GlobalResponse(boolean success, int status, Object data, LocalDateTime timestamp) {
+	public static GlobalResponse success(int status, Object data) {
+		return new GlobalResponse(true, status, data, LocalDateTime.now());
+	}
+
+	public static GlobalResponse fail(int status, ErrorResponse errorResponse) {
+		return new GlobalResponse(false, status,errorResponse, LocalDateTime.now());
+	}
+}
