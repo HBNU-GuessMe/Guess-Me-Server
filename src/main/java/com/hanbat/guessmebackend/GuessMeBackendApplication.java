682187ce9f35fea2c86f59ee5f4c5af3491d2b82
suri-pu-bi <ymg12347@gmail.com>
2024-05-13 16:17:27 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/main/java/com/hanbat/guessmebackend/GuessMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hanbat.guessmebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessMeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessMeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanbat/guessmebackend/GuessMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbat.guessmebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessMeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
