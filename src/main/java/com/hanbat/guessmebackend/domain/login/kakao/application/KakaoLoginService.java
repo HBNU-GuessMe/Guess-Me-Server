63575f3d3215638622f88cebe73195373a4bc5fa
suri-pu-bi <ymg12347@gmail.com>
2024-05-15 20:31:51 +0900

    feat: 회원가입 기능 구현

--- src/main/java/com/hanbat/guessmebackend/domain/login/kakao/application/KakaoLoginService.java
+++ src/main/java/com/hanbat/guessmebackend/domain/login/kakao/application/KakaoLoginService.java
@@ -13,8 +13,12 @@ import org.springframework.web.reactive.function.client.WebClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hanbat.guessmebackend.domain.login.kakao.dto.KakaoToken;
-import com.hanbat.guessmebackend.domain.login.kakao.dto.KakaoUserInfo;
+import com.hanbat.guessmebackend.domain.login.kakao.dto.KakaoTokenResponse;
+import com.hanbat.guessmebackend.domain.login.kakao.dto.OauthUserInfoResponse;
+import com.hanbat.guessmebackend.domain.login.kakao.dto.UserInfoAndTokenResponse;
+import com.hanbat.guessmebackend.domain.user.entity.SnsType;
+import com.hanbat.guessmebackend.domain.user.entity.User;
+import com.hanbat.guessmebackend.domain.user.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class KakaoLoginService {
+	private final UserRepository userRepository;
 
 	private final String reqURL = "https://kauth.kakao.com/oauth/token";
 	private final String userInfoURL = "https://kapi.kakao.com/v2/user/me";
@@ -40,7 +45,7 @@ public class KakaoLoginService {
 	/**
 	 * 인가코드를 카카오에 요청해 엑세스 토큰을 받는 함수
 	 */
-	public KakaoToken getAccessToken(String code) throws IOException {
+	public KakaoTokenResponse getAccessToken(String code) throws IOException {
 		// 파라미터 세팅
 		MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
 		params.add("client_id", clientId);
@@ -63,24 +68,23 @@ public class KakaoLoginService {
 
 		// json 형태로 변환
 		ObjectMapper mapper = new ObjectMapper();
-		KakaoToken kakaoToken = null;
+		KakaoTokenResponse kakaoTokenResponse = null;
 
 		try {
 
-			kakaoToken = mapper.readValue(response, KakaoToken.class);
+			kakaoTokenResponse = mapper.readValue(response, KakaoTokenResponse.class);
 		} catch (JsonProcessingException e) {
 			throw new RuntimeException(e);
 		}
 
-		return kakaoToken;
+		return kakaoTokenResponse;
 
 	}
 
 	/**
 	 *  카카오 사용자 정보 가져오기
 	 */
-
-	public KakaoUserInfo getUserInfo(String token) throws JsonProcessingException {
+	public OauthUserInfoResponse getUserInfo(String token) throws JsonProcessingException {
 
 		// 유저정보 GET 요청
 		WebClient webClient = WebClient.create(userInfoURL);
@@ -95,6 +99,21 @@ public class KakaoLoginService {
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode jsonNode = objectMapper.readTree(response);
 		log.info(jsonNode.asText());
-		return new KakaoUserInfo(jsonNode.get("id").asLong(), jsonNode.get("kakao_account").get("email").asText());
+		return new OauthUserInfoResponse(jsonNode.get("id").asLong(), jsonNode.get("kakao_account").get("email").asText());
+	}
+
+	/*
+		로그인한 사용자 회원가입
+	 */
+	@Transactional
+	public UserInfoAndTokenResponse signup(String accessToken, String email) {
+		User user = userRepository.save(new User(email, SnsType.KAKAO));
+		return UserInfoAndTokenResponse.builder()
+			.userId(user.getId())
+			.email(email)
+			.snsType(user.getSnsType())
+			.accessToken(accessToken)
+			.build();
+
 	}
 }
