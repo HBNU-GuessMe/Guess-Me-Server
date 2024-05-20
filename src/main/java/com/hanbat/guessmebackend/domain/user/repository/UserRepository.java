d021c82f4dca63a8626df9250187d7a96a5baa4a
suri-pu-bi <ymg12347@gmail.com>
2024-05-20 20:46:00 +0900

    feat: findBySnsId, existsBySnsId 함수 추가

--- src/main/java/com/hanbat/guessmebackend/domain/user/repository/UserRepository.java
+++ src/main/java/com/hanbat/guessmebackend/domain/user/repository/UserRepository.java
@@ -1,8 +1,12 @@
 package com.hanbat.guessmebackend.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hanbat.guessmebackend.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	Boolean existsBySnsId(String snsId);
+	User findBySnsId(String snsId);
 }
