fb588cd1d35d1fcab7e1f84db12205dc8a4c61d4
suri-pu-bi <ymg12347@gmail.com>
2024-05-26 16:28:05 +0900

    feat: 코드 일치하지 않을때 에러코드 설정

--- src/main/java/com/hanbat/guessmebackend/global/error/exception/ErrorCode.java
+++ src/main/java/com/hanbat/guessmebackend/global/error/exception/ErrorCode.java
@@ -23,6 +23,7 @@ public enum ErrorCode {
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
 	USER_SOCIAL_INFO_NOT_FOUND(HttpStatus.NOT_FOUND, "소셜 정보를 찾을 수 없습니다."),
 	USER_ALREADY_REGISTERED(HttpStatus.CONFLICT, "이미 가입된 유저입니다."),
+	USER_CODE_NOT_MATCHED(HttpStatus.BAD_REQUEST, "코드가 일치하지않습니다."),
 
 
 	// Family
