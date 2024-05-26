2d2553473e470db858ef331c400b3966f8cee4e1
suri-pu-bi <ymg12347@gmail.com>
2024-05-26 21:03:15 +0900

    feat: familyId로 해당 가족에 속한 유저들을 반환하는 쿼리 작성

--- src/main/java/com/hanbat/guessmebackend/domain/family/repository/FamilyRepository.java
+++ src/main/java/com/hanbat/guessmebackend/domain/family/repository/FamilyRepository.java
@@ -1,9 +1,17 @@
 package com.hanbat.guessmebackend.domain.family.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.hanbat.guessmebackend.domain.family.entity.Family;
+import com.hanbat.guessmebackend.domain.user.entity.User;
 
 public interface FamilyRepository extends JpaRepository<Family, Long> {
+	@Query("SELECT u FROM User u JOIN u.family f WHERE f.id = :familyId" )
+	Optional<List<User>> findUsersByFamilyId(@Param("familyId") Long familyId);
 
 }
