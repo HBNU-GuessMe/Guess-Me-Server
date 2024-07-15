902c27756fad698ea8397b983747adf5d607a5a1
suri-pu-bi <ymg12347@gmail.com>
2024-07-15 11:34:22 +0900

    feat: chat 엔티티 설정

--- src/main/java/com/hanbat/guessmebackend/domain/chat/entity/Chat.java
+++ src/main/java/com/hanbat/guessmebackend/domain/chat/entity/Chat.java
@@ -1,12 +1,21 @@
 package com.hanbat.guessmebackend.domain.chat.entity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Document(collation = "chat")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Chat {
 	@Id
 	@Field(targetType = FieldType.OBJECT_ID)
@@ -22,6 +31,21 @@ public class Chat {
 	private String senderType;
 
 	private String content;
+
+	private LocalDateTime sentAt;
+
+	@Builder
+	public Chat(Long roomId, Long userId, String senderType, String content, LocalDateTime sentAt) {
+		this.roomId = roomId;
+		this.userId = userId;
+		this.senderType = senderType;
+		this.content = content;
+		this.sentAt = sentAt;
+	}
+
+
+
+
 
 
 }
