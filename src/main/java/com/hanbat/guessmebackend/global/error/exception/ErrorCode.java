629ce6610c15f7c914c5ee49ec7c43f10335e226
suri-pu-bi <ymg12347@gmail.com>
2024-07-15 09:24:33 +0900

    feat: 채팅방 에러코드 설정

--- src/main/java/com/hanbat/guessmebackend/global/error/exception/ErrorCode.java
+++ src/main/java/com/hanbat/guessmebackend/global/error/exception/ErrorCode.java
@@ -46,7 +46,7 @@ public enum ErrorCode {
 
 
 	// Chat
-	CHAT_ROOM_NOT_CREATED_YET(HttpStatus.BAD_REQUEST, "아직 채팅방이 생성되지 않았습니다.");
+	CHAT_ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 채팅방을 찾을 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String message;
