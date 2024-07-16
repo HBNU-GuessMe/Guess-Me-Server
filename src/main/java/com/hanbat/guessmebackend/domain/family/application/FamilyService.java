8c36b0f44f9240ddc7b8bf6ad3932c20951dcfae
suri-pu-bi <ymg12347@gmail.com>
2024-07-16 18:14:59 +0900

    feat: 가족 코드 Owner가 가족 연결하는 로직과 다른 가족 구성원이 추가로 조인하는 로직 구현

--- src/main/java/com/hanbat/guessmebackend/domain/family/application/FamilyService.java
+++ src/main/java/com/hanbat/guessmebackend/domain/family/application/FamilyService.java
@@ -3,13 +3,12 @@ package com.hanbat.guessmebackend.domain.family.application;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.hanbat.guessmebackend.domain.family.dto.FamilyInfoRequest;
-import com.hanbat.guessmebackend.domain.family.dto.FamilyInfoResponse;
+import com.hanbat.guessmebackend.domain.family.dto.FamilyConnectionRequest;
+import com.hanbat.guessmebackend.domain.family.dto.FamilyConnectionWithOwnerRequest;
 import com.hanbat.guessmebackend.domain.family.dto.FamilyUserInfoResponse;
 import com.hanbat.guessmebackend.domain.family.entity.Family;
 import com.hanbat.guessmebackend.domain.family.repository.FamilyRepository;
@@ -32,36 +31,90 @@ public class FamilyService {
 	private final UserRepository userRepository;
 	private final MemberUtil memberUtil;
 
+	/*
+		가족 코드 owner가 가족 연결
+	 */
 	@Transactional
-	public FamilyInfoResponse connectFamily(FamilyInfoRequest familyInfoRequest) {
+	public FamilyUserInfoResponse connectFamilyWithOwner(
+		FamilyConnectionWithOwnerRequest familyConnectionWithOwnerRequest) {
 		final User ownerUser = memberUtil.getCurrentUser();
 
-		Optional.of(familyInfoRequest.getCode())
+		// request로 받은 코드와 연결하기를 누른 사용자의 코드가 일치하는 확인 (코드 사용자만 연결하기 누르기 가능)
+		Optional.of(familyConnectionWithOwnerRequest.getCode())
 			.filter((c) -> c.equals(ownerUser.getUserCode()))
 			.ifPresentOrElse(
 				c -> {},
 				() -> { throw new CustomException(ErrorCode.FAMILY_CODE_IS_NOT_OWNER);});
 
-		int count = familyInfoRequest.getUserIds().size();
+
+		// owner가 연결을 하려는데, 이미 가족이 존재한다면, 에러 발생
+		if (familyRepository.existsFamilyByFamilyCode(familyConnectionWithOwnerRequest.getCode())) {
+			throw new CustomException(ErrorCode.FAMILY_ALREADY_REGISTERED);
+		}
+
+		// 가족이 존재하지않는다면, 가족 저장 후 해당 유저들의 family 업데이트
+		// 연결시킨 가족 구성원 수 + 가족코드 소유자 1명
+		int count = familyConnectionWithOwnerRequest.getUserIds().size() + 1;
 
 		Family family = Family.builder()
-			.familyCode(familyInfoRequest.getCode())
+			.familyCode(familyConnectionWithOwnerRequest.getCode())
 			.count(count)
 			.build();
 
 		familyRepository.save(family);
 
-		familyInfoRequest.getUserIds().forEach(userId -> {
+		familyConnectionWithOwnerRequest.getUserIds().forEach(userId -> {
 			User user = userRepository.findById(userId)
 				.orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 			user.updateFamily(family);
-			userRepository.save(user);
 		});
 
+		ownerUser.updateFamily(family);
 
-		return FamilyInfoResponse.fromFamily(family, familyInfoRequest.getUserIds());
+		List<User> users = familyRepository.findUsersByFamilyId(family.getId())
+			.orElseThrow(() -> new CustomException(ErrorCode.FAMILY_AND_USERS_NOT_FOUND));
+
+		List<UserCommonInfoResponse> userResponses = users.stream()
+			.map(UserCommonInfoResponse::fromUser)
+			.toList();
+
+
+		return FamilyUserInfoResponse.fromFamily(family, userResponses);
 
 	}
+
+	/*
+		다른 가족 구성원이 가족에 추가로 조인
+		가족이 존재한다면, 가족을 찾아서 count 업데이트하고, 해당 유저의 family 업데이트
+	 */
+	@Transactional
+	public FamilyUserInfoResponse connectFamily(FamilyConnectionRequest familyConnectionRequest) {
+		final User user = memberUtil.getCurrentUser();
+
+		Family family = familyRepository.findFamilyByFamilyCode(familyConnectionRequest.getCode())
+			.orElseThrow(() -> new CustomException(ErrorCode.FAMILY_NOT_FOUND));
+
+		// 이미 존재하는 가족의 구성원 수 + 새로 들어운 가족 구성원 수
+		int count = family.getCount() + 1;
+
+		family.updateCount(count);
+
+		user.updateFamily(family);
+
+		List<User> users = familyRepository.findUsersByFamilyId(family.getId())
+			.orElseThrow(() -> new CustomException(ErrorCode.FAMILY_AND_USERS_NOT_FOUND));
+
+		List<UserCommonInfoResponse> userResponses = users.stream()
+			.map(UserCommonInfoResponse::fromUser)
+			.toList();
+
+
+		return FamilyUserInfoResponse.fromFamily(family, userResponses);
+	}
+
+	/*
+		가족 조회
+	 */
 
 	public FamilyUserInfoResponse getFamilyInfo(Long familyId) {
 
@@ -77,6 +130,8 @@ public class FamilyService {
 
 		return FamilyUserInfoResponse.fromFamily(family, userResponses);
 	}
+
+
 
 
 }
