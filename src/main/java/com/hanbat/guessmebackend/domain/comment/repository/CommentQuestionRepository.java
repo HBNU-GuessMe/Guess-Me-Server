544cc6077d21a991ec73e94171ccfd400d5f7141
suri-pu-bi <ymg12347@gmail.com>
2024-07-17 13:12:27 +0900

    feat: CommentAnswer에서 CommentQuestion으로 수정

--- src/main/java/com/hanbat/guessmebackend/domain/comment/repository/CommentQuestionRepository.java
+++ src/main/java/com/hanbat/guessmebackend/domain/comment/repository/CommentQuestionRepository.java
@@ -2,7 +2,7 @@ package com.hanbat.guessmebackend.domain.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.hanbat.guessmebackend.domain.comment.entity.CommentAnswer;
+import com.hanbat.guessmebackend.domain.comment.entity.CommentQuestion;
 
-public interface CommentQuestionRepository extends JpaRepository<CommentAnswer, Long> {
+public interface CommentQuestionRepository extends JpaRepository<CommentQuestion, Long> {
 }
