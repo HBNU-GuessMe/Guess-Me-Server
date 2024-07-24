06114aee353aad026c0acc941705857a1ca80db4
suri-pu-bi <ymg12347@gmail.com>
2024-07-25 03:53:43 +0900

    feat: answer dto 수정

--- src/main/java/com/hanbat/guessmebackend/domain/answer/dto/AnswerGetAllResponse.java
+++ src/main/java/com/hanbat/guessmebackend/domain/answer/dto/AnswerGetAllResponse.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 import com.hanbat.guessmebackend.domain.question.entity.Question;
 
-public record AnswerGetAllResponse(Long questionId, Long familyId, List<AnswerGetResponse> answers) {
+public record AnswerGetAllResponse(Long questionId, String questionContent, Long familyId, List<AnswerGetResponse> answers) {
 
 	public static AnswerGetAllResponse fromAnswer(Question question, List<AnswerGetResponse> answers) {
-		return new AnswerGetAllResponse(question.getId(), question.getFamily().getId(), answers);
+		return new AnswerGetAllResponse(question.getId(), question.getContent(), question.getFamily().getId(), answers);
 	}
 }
