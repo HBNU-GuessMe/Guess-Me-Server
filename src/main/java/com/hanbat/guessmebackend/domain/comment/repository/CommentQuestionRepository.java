2bc784fd50df2919ea31df6d386dcf0747e518ea
suri-pu-bi <ymg12347@gmail.com>
2024-07-25 04:05:56 +0900

    feat: commentAnswerCount의 합계를 찾는 쿼리, familyId로 commentQuestion들을 모두 찾는 쿼리 작성

--- src/main/java/com/hanbat/guessmebackend/domain/comment/repository/CommentQuestionRepository.java
+++ src/main/java/com/hanbat/guessmebackend/domain/comment/repository/CommentQuestionRepository.java
@@ -1,8 +1,26 @@
 package com.hanbat.guessmebackend.domain.comment.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.hanbat.guessmebackend.domain.comment.entity.CommentQuestion;
 
-public interface CommentQuestionRepository extends JpaRepository<CommentQuestion, Long> {
+public interface CommentQuestionRepository extends JpaRepository<CommentQuestion, Long>,  CommentQuestionRepositoryCustom {
+
+	@Query("SELECT SUM(cq.commentAnswerCount) FROM CommentQuestion cq " +
+		"JOIN cq.question q " +
+		"WHERE q.id = :questionId")
+	Integer findSumOfCommentAnswerCount(@Param("questionId") Long questionId);
+
+	@Query("SELECT cq, ca FROM CommentQuestion cq " +
+		"LEFT JOIN cq.commentAnswer ca " +
+		"JOIN cq.question q " +
+		"JOIN q.family f " +
+		"WHERE f.id = :familyId")
+	List<Object[]> findCommentQuestionsAndAnswersByFamilyId(@Param("familyId") Long familyId);
+
 }
