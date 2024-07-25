0b8c27a4b98721dd34a7fd5e143c7c1567e4c9c8
suri-pu-bi <ymg12347@gmail.com>
2024-07-25 16:23:58 +0900

    feat: 채팅 저장 및 조회 로직 구현

--- src/main/java/com/hanbat/guessmebackend/domain/chat/application/ChatService.java
+++ src/main/java/com/hanbat/guessmebackend/domain/chat/application/ChatService.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -30,6 +32,7 @@ import com.hanbat.guessmebackend.domain.comment.application.CommentService;
 import com.hanbat.guessmebackend.domain.comment.dto.CommentsByFamilyGetResponse;
 import com.hanbat.guessmebackend.domain.question.application.ChatgptQuestionService;
 import com.hanbat.guessmebackend.domain.user.entity.User;
+import com.hanbat.guessmebackend.domain.user.repository.UserRepository;
 import com.hanbat.guessmebackend.global.error.exception.CustomException;
 import com.hanbat.guessmebackend.global.error.exception.ErrorCode;
 import com.hanbat.guessmebackend.global.jwt.MemberUtil;
@@ -50,28 +53,43 @@ public class ChatService {
 
 	private final ChatRepository chatRepository;
 	private final ChatRoomRepository chatRoomRepository;
+	private final UserRepository userRepository;
 	private final MemberUtil memberUtil;
 	private final SimpMessagingTemplate simpMessagingTemplate;
 	private final ChatgptQuestionService chatgptQuestionService;
 	private final CommentService commentService;
 
-	/*
-		채팅을 mongodb에 저장
-	 */
-
 
 	/*
-		채팅방 클릭 시, 사용자가 읽지않은 것부터 조회하고, 읽음처리
-
+		채팅방 메세지 저장하기
 	 */
+	@Transactional
+	public void createChat(Message message) {
+		Chat chat = Chat.builder()
+			.userId(message.getSenderId())
+			.roomId(message.getRoomId())
+			.content(message.getContent())
+			.sentAt(LocalDateTime.now())
+			.senderType("사용자")
+			.build();
 
-	/*
-		채팅방에 사용자가 접속 중인지 확인 (readCount = 0)
-	 */
+		chatRepository.save(chat);
+	}
 
-	/*
-		채팅방에 사용자가 접속 중이 아니라면 (readCount = 1)
-	 */
+	public List<ChatResponse> getChats(Long chatroomId) {
+		Sort sort = Sort.by(Sort.Direction.ASC, "createdAt");
+		List<Chat> chats = chatRepository.findChatsByRoomId(chatroomId, sort);
+		List<ChatResponse> chatResponses = new ArrayList<>();
+		for (Chat chat : chats) {
+			Optional<User> userOptional = userRepository.findById(chat.getUserId());
+
+			// User 객체가 존재하는 경우 닉네임을 가져오고, 그렇지 않은 경우 null을 설정
+			String nickname = userOptional.map(User::getNickname).orElse(null);
+
+			chatResponses.add(ChatResponse.fromChat(chat, nickname));
+		}
+		return chatResponses;
+	}
 
 	/*
 		해피가 채팅방에 메세지 전송
@@ -118,13 +136,13 @@ public class ChatService {
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> message1 = new HashMap<>();
 		message1.put("role", "system");
-		message1.put("content", "당신은 가족 상담을 하기위한 전문의입니다.");
+		message1.put("content", "당신은 가족 상담을 하기위한 상담가입니다. 말투는 부드럽게, 존댓말로 해주세요.");
 
 		Map<String, Object> message2 = new HashMap<>();
 		message2.put("role", "user");
 		List<CommentsByFamilyGetResponse> responses = commentService.getAllCommentsByFamily(familyId);
 		JsonNode jsonResponses = mapper.convertValue(responses, JsonNode.class);
-		message2.put("content", jsonResponses + "가족상담을 위한 전문의로써 위 json 파일을 바탕으로 상담을 이어갈 질문 1개를 구체적으로 생성해줘.");
+		message2.put("content", jsonResponses + "가족상담을 위한 전문의로써 위 json 파일을 바탕으로 길게 구체적으로 상담할 질문을 생성해주세요.");
 
 		List<Map<String, Object>> messages = new ArrayList<>();
 		messages.add(message1);
